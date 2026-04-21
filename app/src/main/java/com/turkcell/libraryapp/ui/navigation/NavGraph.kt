7ef5c0ff9da0eedea8504a7f70a2d079e8da78c7
halitklyc <halitenes.kalayci@gmail.com>
2026-04-21 19:56:42 +0300

    add login screen

--- app/src/main/java/com/turkcell/libraryapp/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/turkcell/libraryapp/ui/navigation/NavGraph.kt
@@ -6,12 +6,13 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.turkcell.libraryapp.ui.screen.LoginScreen
 
 @Composable
 fun NavGraph(navController: NavHostController = rememberNavController()) {
     NavHost(navController = navController, startDestination = Screen.Login.route)
     {
-        composable(Screen.Login.route) { Text("Giriş Sayfası") }
+        composable(Screen.Login.route) { LoginScreen() }
         composable(Screen.Register.route) { Text("Kayıt Ol Sayfası")  }
     }
 }
