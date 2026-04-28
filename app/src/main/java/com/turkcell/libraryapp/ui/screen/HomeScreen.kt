fdbb8f5b1ba48e2c3d7fddeb6e60070d39800de0
halitklyc <halitenes.kalayci@gmail.com>
2026-04-28 20:56:48 +0300

    28 nisan'ders sonu

--- app/src/main/java/com/turkcell/libraryapp/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/turkcell/libraryapp/ui/screen/HomeScreen.kt
@@ -2,8 +2,11 @@ package com.turkcell.libraryapp.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,7 +16,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.turkcell.libraryapp.ui.viewmodel.AuthViewModel
 import com.turkcell.libraryapp.ui.viewmodel.BookViewModel
 
@@ -27,13 +29,22 @@ fun HomeScreen(
     val isLoading by bookViewModel.isLoading.collectAsState();
 
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-        if (isLoading)
-        {
-            CircularProgressIndicator(modifier=Modifier.size(20.dp),
+        when {
+            isLoading ->  CircularProgressIndicator(modifier=Modifier.size(20.dp),
                 strokeWidth = 2.dp,
                 color = MaterialTheme.colorScheme.onPrimary)
-        }else{
-            Text("Yüklendi ${books.count()}")
+            books.isEmpty() -> Text("Kitaplar yüklenemedi.")
+            else -> LazyColumn(modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(32.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                items(books, key = {it.id})
+                {
+                    // ÖDEV 3: Bir "Kitap" kart tasarımı (ayrı composable) buradaki listede
+                    // doldurulsun.
+                    book ->
+                    Text(book.title)
+                }
+            }
         }
     }
 }
